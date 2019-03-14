db89d106585d380aa916aa5b681b072f5b1b2301
yuuki takezawa <yuuki.takezawa@comnect.jp.net>
2019-03-14 12:17:51 +0900

    updated for 5.8 (#59)

--- tests/MockApplication.php
+++ tests/MockApplication.php
@@ -32,14 +32,7 @@ class MockApplication extends \Illuminate\Container\Container implements \Illumi
         // TODO: Implement version() method.
     }
 
-    /**
-     * Get or check the current application environment.
-     *
-     * @param  mixed
-     *
-     * @return string
-     */
-    public function environment()
+    public function environment(...$environments)
     {
         // TODO: Implement environment() method.
     }
@@ -154,6 +147,128 @@ class MockApplication extends \Illuminate\Container\Container implements \Illumi
     {
         // TODO: Implement getCachedServicesPath() method.
     }
+
+    public function bootstrapPath($path = '')
+    {
+        // TODO: Implement bootstrapPath() method.
+    }
+
+    public function configPath($path = '')
+    {
+        // TODO: Implement configPath() method.
+    }
+
+    public function databasePath($path = '')
+    {
+        // TODO: Implement databasePath() method.
+    }
+
+    public function environmentPath()
+    {
+        // TODO: Implement environmentPath() method.
+    }
+
+    public function resourcePath($path = '')
+    {
+        // TODO: Implement resourcePath() method.
+    }
+
+    public function storagePath()
+    {
+        // TODO: Implement storagePath() method.
+    }
+
+    public function resolveProvider($provider)
+    {
+        // TODO: Implement resolveProvider() method.
+    }
+
+    public function bootstrapWith(array $bootstrappers)
+    {
+        // TODO: Implement bootstrapWith() method.
+    }
+
+    public function configurationIsCached()
+    {
+        // TODO: Implement configurationIsCached() method.
+    }
+
+    public function detectEnvironment(Closure $callback)
+    {
+        // TODO: Implement detectEnvironment() method.
+    }
+
+    public function environmentFile()
+    {
+        // TODO: Implement environmentFile() method.
+    }
+
+    public function environmentFilePath()
+    {
+        // TODO: Implement environmentFilePath() method.
+    }
+
+    public function getCachedConfigPath()
+    {
+        // TODO: Implement getCachedConfigPath() method.
+    }
+
+    public function getCachedRoutesPath()
+    {
+        // TODO: Implement getCachedRoutesPath() method.
+    }
+
+    public function getLocale()
+    {
+        // TODO: Implement getLocale() method.
+    }
+
+    public function getNamespace()
+    {
+        // TODO: Implement getNamespace() method.
+    }
+
+    public function getProviders($provider)
+    {
+        // TODO: Implement getProviders() method.
+    }
+
+    public function hasBeenBootstrapped()
+    {
+        // TODO: Implement hasBeenBootstrapped() method.
+    }
+
+    public function loadDeferredProviders()
+    {
+        // TODO: Implement loadDeferredProviders() method.
+    }
+
+    public function loadEnvironmentFrom($file)
+    {
+        // TODO: Implement loadEnvironmentFrom() method.
+    }
+
+    public function routesAreCached()
+    {
+        // TODO: Implement routesAreCached() method.
+    }
+
+    public function setLocale($locale)
+    {
+        // TODO: Implement setLocale() method.
+    }
+
+    public function shouldSkipMiddleware()
+    {
+        // TODO: Implement shouldSkipMiddleware() method.
+    }
+
+    public function terminate()
+    {
+        // TODO: Implement terminate() method.
+    }
+
+
 }
 
 function base_path()
